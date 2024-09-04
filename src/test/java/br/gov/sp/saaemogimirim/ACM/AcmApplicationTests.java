1d031071d7b4349df93cd24683b164a3cbc09146
MARIOHELIOSIMOES <mariohelio_simoes@hotmail.com>
2024-09-04 14:12:25 -0300

    Project created

---
+++ src/test/java/br/gov/sp/saaemogimirim/ACM/AcmApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.saaemogimirim.ACM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
